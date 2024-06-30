2775fc1fbf3a897bbd96b2ed0589c6128eb63672
Pablo Bernabeu <pcbernabeu@gmail.com>
2024-06-30 15:03:06 +0200

    Update count_markers.R

--- count_markers.R
+++ count_markers.R
@@ -1,17 +1,16 @@
 
 
-# Count instances of markers 'S  2' and 'S  3'
+# Count instances of a marker
 
 count_markers = 
   
-  function(file_without_extension, marker) {
+  function(file, marker) {
     
     require(dplyr)
     require(stringr)
     
     # Read the file into a vector of lines
-    file_path = paste0(file_without_extension, '.vmrk')
-    file = readLines(file_path)
+    file = readLines(paste0(file_without_extension, '.vmrk'))
     
     # Count and print result
     marker_count = sum(str_detect(file, 'S  2'))
