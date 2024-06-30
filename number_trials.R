56941225518f89d997e3915e289dda02d3962bd3
Pablo Bernabeu <pcbernabeu@gmail.com>
2024-06-30 14:05:02 +0200

    Create number_trials.R

---
+++ number_trials.R
@@ -0,0 +1,26 @@
+
+
+# Create temporary vmrk file with numbered trials
+
+number_trials = 
+  
+  function(file_without_extension, start_line, lines_per_trial, first_number) {
+    
+    # Read the file into a vector of lines
+    file_path = paste0(file_without_extension, '.vmrk')
+    file = readLines(file_path)
+    
+    # Initialize the ordinal number counter
+    ordinal_number = first_number
+    
+    # Loop through the lines and append the ordinal number 
+    # to every fifth line after the 10th line.
+    for (i in seq(start_line, length(file), lines_per_trial)) {
+      file[i] = paste0(file[i], '  ', ordinal_number)
+      ordinal_number = ordinal_number + 1
+    }
+    
+    # Write the modified lines back to a file
+    output_file_path = paste0(file_without_extension, '_temp.vmrk')
+    writeLines(file, output_file_path)
+  }
