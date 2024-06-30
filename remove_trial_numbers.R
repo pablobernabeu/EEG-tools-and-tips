c1c047ca361a77309e964d1eff130b5fc924770a
Pablo Bernabeu <pcbernabeu@gmail.com>
2024-06-30 14:07:31 +0200

    Create remove_trial_numbers.R

---
+++ remove_trial_numbers.R
@@ -0,0 +1,22 @@
+
+
+# Remove trial numbers from temporary file
+
+remove_trial_numbers = 
+  
+  function(temp_file_without_extension) {
+    
+    require(dplyr)
+    require(stringr)
+    
+    # Read the file into a vector of lines
+    file_path = paste0(temp_file_without_extension, '.vmrk')
+    file = readLines(file_path)
+    
+    # Remove trial numbers
+    file = file %>% str_replace_all(',0  .*', ',0')
+    
+    # Write the modified lines back to a file
+    output_file_path = paste0(temp_file_without_extension, '.vmrk')
+    writeLines(file, output_file_path)
+  }
